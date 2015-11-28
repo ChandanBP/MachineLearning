c1009d28cd07d3922b44366b14fae20a66682163
System Administrator <root@chandans-MacBook-Pro.local>
2015-11-28 16:18:03 -0600

    Tweets Clustering using kmeans

---
+++ TweetsClustering/TweetNode.java
@@ -0,0 +1,26 @@
+package tweetsClustering;
+
+public class TweetNode {
+
+	int clusterID;
+	float dist;
+	String id;
+	String text;
+	String profile_image_url;
+	String from_user;
+	String from_user_id;
+	String geo;
+	String iso_language_code;
+	String from_user_id_str;
+	String created_at;
+	String source;
+	String id_str;
+	String from_user_name;
+	String profile_image_url_https;
+	String[] metadata;
+	
+	public TweetNode(){
+		//clusterID = -1;
+		dist = Float.MAX_VALUE;
+	}
+}

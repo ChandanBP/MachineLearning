c303fa90d12376554b4b310a3889151e6ce2f233
System Administrator <root@chandans-MacBook-Pro.local>
2015-11-28 16:15:24 -0600

    Kmeans Clustering

---
+++ Kmeans/Point.java
@@ -0,0 +1,27 @@
+package kmeans;
+
+
+public class Point{
+   
+	//boolean isChanged;
+	int id;
+	int clusterID;
+	float x;
+    float y;
+    float dist;
+    
+//    @Override
+//    protected Object clone() throws CloneNotSupportedException {
+//    
+//    	Point point = new Point();
+//    	
+//    	point.id = this.id;
+//    	point.clusterID = this.clusterID;
+//    	point.x = this.x;
+//    	point.y = this.y;
+//    	point.dist = this.dist;
+//    	
+//    	return super.clone();
+//    }
+    
+}
